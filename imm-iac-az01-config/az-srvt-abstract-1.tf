a7607c31e4ddd9d26059b33eb0af7a1637ccb9de
bywhite <bywhite@cisco.com>
2023-08-16 16:32:10 -0500

    changed variable name

--- imm-iac-az01-config/az-srvt-abstract-1.tf
+++ imm-iac-az01-config/az-srvt-abstract-1.tf
@@ -4,7 +4,7 @@
 # #    * Note: Derivation of Server Profiles is outside of TF scope
 # # Duplicate this code/file to add another new Server Template and:
 # #    * Change module: "server_template_vmwX"  >> "server_template_vmwY"
-# #    * Change server_policy_prefix: "...-vmwX" > "...-vmwY"
+# #    * Change server_template_name: "...-vmwX" > "...-vmwY"
 # #    * Change Tag value for "ServerGroup" to include new name
 # #    * Modify parameters as needed to tweak your template configuration
 # # -----------------------------------------------------------------------------
@@ -25,7 +25,7 @@ module "server_template_abstract_1" {                                   # <<-- C
 # -----------------------------------------------------------------------------
 
   # prefix for all created policies
-  server_policy_prefix = "${var.az_policy_prefix}-abstract-1"         # <<-- Change to duplicate
+  server_template_name = "${var.az_policy_prefix}-srvt-vmware-01"         # <<-- Change to duplicate
   description   = "built by Terraform ${var.az_policy_prefix}"
 
   #Every object created in the domain will have these tags
@@ -33,20 +33,12 @@ module "server_template_abstract_1" {                                   # <<-- C
     { "key" : "environment", "value" : "dev" },
     { "key" : "orchestrator", "value" : "Terraform" },
     { "key" : "az", "value" : "${var.az_policy_prefix}" },
-    { "key" : "ServerGroup", "value" : "${var.az_policy_prefix}-abstract-1" } # <-- Change to duplicate
+    { "key" : "os-target", "value" : "ESX7-U3" }                          # <-- Change as needed
   ]
-
-# =============================================================================
-# X vs B Chassis Profile Customizations
-# -----------------------------------------------------------------------------
-# Customize policies for X-Series (true) or B-Series (false)
-  is_x_series_profile = true 
-
 
 # =============================================================================
 # az-wide pools
 # -----------------------------------------------------------------------------
-
   mac_pool_moid         = module.imm_az_pools_mod.mac_pool_moid
   imc_ip_pool_moid      = module.imm_az_pools_mod.ip_pool_moid
   wwnn_pool_moid        = module.imm_az_pools_mod.wwnn_pool_moid
@@ -55,11 +47,9 @@ module "server_template_abstract_1" {                                   # <<-- C
   server_uuid_pool_moid = module.imm_az_pools_mod.uuid_pool_moid
   server_uuid_pool_name = module.imm_az_pools_mod.uuid_pool_name
 
-
 # =============================================================================
 # az-wide server policies
 # -----------------------------------------------------------------------------
-
   access_policy     = module.server_policies.access_policy_map["access-1"]
   bios_policy       = module.server_policies.bios_policy_map["bios-1"]
   boot_policy       = module.server_policies.boot_policy_map["boot-1"]
@@ -71,7 +61,9 @@ module "server_template_abstract_1" {                                   # <<-- C
   stor_policy       = "none"   # module.server_policies.stor_policy_map["stor-3"]
   syslog_policy     = "none"   # module.server_policies.syslog_policy_map["syslog-1"]
   vmedia_policy     = "none"   # module.server_policies.vmedia_policy_map["vmedia-1"]
-
+  lancon_policy     = module.server_policies.lancon_policy_map["lancon-4"]
+  sancon_policy     = module.server_policies.sancon_policy_map["sancon-2"]
+  
 # =============================================================================
 # Local IMC Users - defined az wide
 # -----------------------------------------------------------------------------
@@ -79,67 +71,11 @@ module "server_template_abstract_1" {                                   # <<-- C
   user_policy  = module.imm_az_user_policy_1.iam_user_policy_moid
 
 # =============================================================================
-# Server Eth vNic's & FC vHBA's
-# -----------------------------------------------------------------------------
-# Ensure "pci_order is unique and sequential across all vnic/vhba"
-# Ensure vlans & vsans are provisioned on target UCS Domain, use depends_on
-
-  vnic_vlan_sets = {
-    "eth0"  = {
-      vnic_name  = "eth0"
-      native_vlan = 21
-      vlan_range  = "21,60,254,255"
-      switch_id   = "A"
-      pci_order   = 0
-      qos_moid    = module.imm_az_qos_mod.vnic_qos_besteffort_moid
-      adapter     = module.server_policies.adapter_policy_map["adapter-1"]
-      failover    = false
-      netgroup    = module.server_policies.netgroup_policy_map["netgroup-1"]
-      netcontrol  = module.server_policies.netcontrol_policy_map["netcontrol-1"]
-    }
-    "eth1"  = {
-      vnic_name   = "eth1"
-      native_vlan = 21
-      vlan_range  = "21,60,254,255"
-      switch_id   = "B"
-      pci_order   = 1
-      qos_moid    = module.imm_az_qos_mod.vnic_qos_besteffort_moid
-      adapter     = module.server_policies.adapter_policy_map["adapter-1"]
-      failover    = false
-      netgroup    = module.server_policies.netgroup_policy_map["netgroup-1"]
-      netcontrol  = module.server_policies.netcontrol_policy_map["netcontrol-1"]
-    }
-  }
-
-
-  vhba_vsan_sets = {
-    "fc0" = {
-      vhba_name      = "fc0"
-      vsan_moid      = module.imm_az_vsan_policy_1.fc_vsan_101_moid
-      switch_id      = "A"
-      wwpn_pool_moid = module.imm_az_pools_mod.wwpn_pool_a_moid
-      pci_order      = 2
-      qos_moid       = module.imm_az_qos_mod.vnic_qos_fc_moid
-      fcadapter      = module.server_policies.fcadapter_policy_map["fcadapter-1"]
-    }
-    "fc1"  = {
-      vhba_name      = "fc1"
-      vsan_moid      = module.imm_az_vsan_policy_1.fc_vsan_102_moid
-      switch_id      = "B"
-      wwpn_pool_moid = module.imm_az_pools_mod.wwpn_pool_b_moid
-      pci_order      = 3
-      qos_moid       = module.imm_az_qos_mod.vnic_qos_fc_moid
-      fcadapter      = module.server_policies.fcadapter_policy_map["fcadapter-1"]
-    }
-  }
-
-# =============================================================================
 # Dependencies
 # -----------------------------------------------------------------------------
 # The Pools for the az must be created before this domain fabric module executes
   depends_on = [
     module.imm_az_pools_mod, module.imm_az_user_policy_1, module.server_policies,
-    module.imm_az_qos_mod, module.imm_az_vsan_policy_1
   ]
 
 }
